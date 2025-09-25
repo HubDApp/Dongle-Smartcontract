5729b8edcda9625ab21e1ebd21d849d375ba877a
Iwueseiter <156322726+Iwueseiter@users.noreply.github.com>
2025-09-25 11:21:13 +0000

    installations and setup

---
+++ onchain/target/debug/deps/hello_rust-600075ba4fb2b3eb.d
@@ -0,0 +1,5 @@
+/workspaces/Dongle-Smartcontract/hello-rust/target/debug/deps/hello_rust-600075ba4fb2b3eb.d: src/main.rs
+
+/workspaces/Dongle-Smartcontract/hello-rust/target/debug/deps/hello_rust-600075ba4fb2b3eb: src/main.rs
+
+src/main.rs:
---
+++ onchain/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/workspaces/Dongle-Smartcontract/hello-rust/target/debug/hello-rust: /workspaces/Dongle-Smartcontract/hello-rust/src/main.rs
